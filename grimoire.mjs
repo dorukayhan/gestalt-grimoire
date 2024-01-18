30a1021757e0ad5c4bd01cb1d32cb343b926a5a1
dorukayhan <dorukayhan@gmail.com>
2024-01-18 14:55:22 -0500

    Ok now I actually did a thing (bot that says hi)
    Should probably use twurple/chat directly instead of twurple/easy-bot

--- grimoire.mjs
+++ grimoire.mjs
@@ -1,15 +1,49 @@
-import http from 'node:http';
+import { RefreshingAuthProvider } from '@twurple/auth';
+import { Bot, createBotCommand } from '@twurple/easy-bot';
+import { promises as fs } from 'fs';
 
-const hostname = '127.0.0.1';
-const port = 21727;
-
-const server = http.createServer((request, response) => {
-    console.log(`Hit! ${request.headers['user-agent']}`);
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.write('Hello World\n');
-    response.write(`It's currently ${new Date()}\n`);
-    response.end(request.headers['user-agent']);
+/**
+ * various settings and options and knobs
+ */
+const settings = JSON.parse(await fs.readFile("conf/settings.json", "utf-8"));
+/**
+ * settings that are a bad idea to put in a public repo
+ * contains streamerUsername (your username, string), botUsername (string),
+ * clientId (string), clientSecret (string), streamSafeTerminal
+ * (whether the terminal is safe to show on stream, boolean, see auth.onRefresh)
+ */
+const secrets = JSON.parse(await fs.readFile("conf/secrets.json", "utf-8"));
+/**
+ * access and refresh tokens
+ * see step 7 of https://twurple.js.org/docs/examples/chat/basic-bot.html
+ */
+const tokens = JSON.parse(await fs.readFile("conf/tokens.json", "utf-8"));
+const auth = new RefreshingAuthProvider({
+    clientId: secrets.clientId,
+    clientSecret: secrets.clientSecret
 });
 
-server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}`));
+auth.onRefresh(async (userId, newToken) => {
+    console.log("refreshing access token, check tokens.json");
+    if (!(secrets.streamSafeTerminal ?? true))
+        console.log(JSON.stringify(newToken)); // in case writeFile fails
+    await fs.writeFile("conf/tokens.json", JSON.stringify(newToken, null, 4), "utf-8");
+});
+await auth.addUserForToken(tokens, ["chat"]);
+
+/** the */
+const bot = new Bot({
+    authProvider: auth,
+    channels: [secrets.streamerUsername],
+    commands: [
+        createBotCommand("spinrightround", (params, ctx) => {
+            console.log(`getting rotated by ${ctx.userName}`);
+            ctx.say("owoSpin YaeSpin AINTNOWAY");
+        })
+    ]
+});
+
+bot.onJoin((joinEvent) => {
+    console.log(`joined ${joinEvent.broadcasterName}'s chat`);
+    bot.say(joinEvent.broadcasterName, "Hellowo");
+});
